2ee87bcbc975cf884ca80520668b69bf7c80265a
Marcell Szokedencsi <szokedencsim@gmail.com>
2024-11-09 16:41:07 +0100

    Added getters and setters to the base model

--- autoop/core/ml/model/model.py
+++ autoop/core/ml/model/model.py
@@ -1,4 +1,3 @@
-# autoop/core/ml/model/model.py
 from abc import ABC, abstractmethod
 import numpy as np
 from copy import deepcopy
@@ -8,10 +7,33 @@ from autoop.core.ml.artifact import Artifact
 
 class Model(ABC):
     """ A class representing a machine learning model. """
+
     def __init__(self, model_type: Literal["classification", "regression"]):
         """ Initialize the model. """
-        self.model_type = model_type
-        self.parameters: Dict[str, Any] = {}
+        self._model_type = model_type
+        self._parameters: Dict[str, Any] = {}
+
+    @property
+    def model_type(self) -> str:
+        """ Get the model type. """
+        return self._model_type
+
+    @model_type.setter
+    def model_type(self, model_type: str) -> None:
+        """ Set the model type. """
+        if model_type not in ["classification", "regression"]:
+            raise ValueError(
+                "model_type must be either 'classification' or 'regression'")
+        self._model_type = model_type
+
+    @property
+    def parameters(self) -> Dict[str, Any]:
+        """ Get the parameters of the model. """
+        return deepcopy(self._parameters)
+
+    def set_params(self, **params: Any) -> None:
+        """ Set the parameters of the model. """
+        self._parameters.update(params)
 
     @abstractmethod
     def fit(self, X: np.ndarray, y: np.ndarray):
@@ -25,26 +47,22 @@ class Model(ABC):
 
     def get_params(self) -> Dict[str, Any]:
         """ Get the parameters of the model. """
-        return deepcopy(self.parameters)
-
-    def set_params(self, **params: Any) -> None:
-        """ Set the parameters of the model. """
-        self.parameters.update(params)
+        return deepcopy(self._parameters)
 
     def to_artifact(self, name: str) -> Artifact:
         """ Convert the model to an artifact. """
         data = {
-            "model_type": self.model_type,
-            "parameters": self.parameters,
+            "model_type": self._model_type,
+            "parameters": self._parameters,
         }
         return Artifact(name=name, data=data)
 
     def save(self, path: str) -> None:
         """ Save the model to a file. """
         with open(path, 'wb') as file:
-            np.save(file, self.parameters)
+            np.save(file, self._parameters)
 
     def load(self, path: str) -> None:
         """ Load the model from a file. """
         with open(path, 'rb') as file:
-            self.parameters = np.load(file, allow_pickle=True).item()
+            self._parameters = np.load(file, allow_pickle=True).item()
